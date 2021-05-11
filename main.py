1df1be36ea53a4d35197e4ef0e402cc061d24632
Anastasia Stepanova <anstepanova@griddynamics.com>
2021-05-11 14:19:49 +0400

    added tests for k-mxt

--- main.py
+++ main.py
@@ -15,53 +15,52 @@ import cProfile
 import re
 
 
-# logger = logging.getLogger('k_mxt_w')
-#
-#
-# def main():
-#     logger.setLevel(logging.INFO)
-#     fh = logging.FileHandler('k_mxt_w.log')
-#     formatter = logging.Formatter('%(filename)s func:%(funcName)s [LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]' +
-#                                   '%(message)s')
-#     fh.setFormatter(formatter)
-#     logger.addHandler(fh)
-#     logger.info('Program started')
-#     try:
+logger = logging.getLogger('k_mxt_w')
+
+
+def main():
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler('k_mxt_w.log')
+    formatter = logging.Formatter('%(filename)s func:%(funcName)s [LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]' +
+                                  '%(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.info('Program started')
+    try:
 #         # filename = '../data/homegate_month_living_dropna.csv'
 #         # data_property = data.DataPropertyImportSpace(filename=filename)
 #         #
 #         # features_list = ['price']
 #         # x, y, features = data_property.get_data(features_list=features_list)
-#         for k in [9]:
-#             for eps in [0.8]:
-#                 coord, labels = sklearn.datasets.make_blobs(n_samples=100, random_state=0, cluster_std=0.5)
-#                 clusters = clusters_data.ClustersDataSpace2d(x_init=coord[:, 0], y_init=coord[:, 1], metrics='euclidean')
-#                 alg = clustering_algorithms.K_MXT_gauss(k=k, eps=eps, clusters_data=clusters)
-#                 start_time = time.time()
-#                 alg()
-#                 end_time = time.time()
-#                 print(f'k-{k}, eps-{eps}, time-{end_time - start_time}')
-#                 print(f'ARI-{sklearn.metrics.cluster.adjusted_rand_score(clusters.cluster_numbers, labels)}')
-#         #         data.DataSave.arrays_to_csv(source_filename = filename,
-#         #                                     new_filename=f'./results/{os.path.basename(filename)}_k_{k}_eps_{eps}.csv',
-#         #                                     cluster_numbers=clusters.cluster_numbers)
-#         # draw.Draw.draw_points(filename=filename, lat='latitude', lon='longitude', hover_name='price',
-#         #                       hover_data=['rooms', 'url', 'property_type'],
-#         #                       color='price',
-#         #                       color_continuous_scale=px.colors.cyclical.HSV)
-#         # draw.Draw.draw_points(filename=filename, lat='latitude', lon='longitude',
-#         #                       hover_name='rent_per_room',
-#         #                       hover_data=['price', 'rooms'],
-#         #                       color='rent_per_room',
-#         #                       )
-#
-#     except BaseException as e:
-#         logger.error(e, exc_info=True)
-#     logger.info('Done!')
-#
-#
-# if __name__ == '__main__':
-#     main()
+        for k in [9]:
+            for eps in [0.4]:
+                coord, labels = sklearn.datasets.make_moons(n_samples=50, noise=0.05, random_state=0)
+                clusters = clusters_data.ClustersDataSpace2d(x_init=coord[:, 0], y_init=coord[:, 1], metrics='euclidean')
+                alg = clustering_algorithms.K_MXT_gauss(k=k, eps=eps, clusters_data=clusters)
+                start_time = time.time()
+                alg()
+                end_time = time.time()
+                print(clusters.cluster_numbers)
+                print(f'k-{k}, eps-{eps}, time-{end_time - start_time}')
+                print(f'ARI-{sklearn.metrics.cluster.adjusted_rand_score(clusters.cluster_numbers, labels)}')
+        #         data.DataSave.arrays_to_csv(source_filename = filename,
+        #                                     new_filename=f'./results/{os.path.basename(filename)}_k_{k}_eps_{eps}.csv',
+        #                                     cluster_numbers=clusters.cluster_numbers)
+        # draw.Draw.draw_points(filename=filename, lat='latitude', lon='longitude', hover_name='price',
+        #                       hover_data=['rooms', 'url', 'property_type'],
+        #                       color='price',
+        #                       color_continuous_scale=px.colors.cyclical.HSV)
+        # draw.Draw.draw_points(filename=filename, lat='latitude', lon='longitude',
+        #                       hover_name='rent_per_room',
+        #                       hover_data=['price', 'rooms'],
+        #                       color='rent_per_room',
+        #                       )
 
-array = np.array([1])
-print(array, clusters_data.ClustersData.array_rationing(array))
+    except BaseException as e:
+        logger.error(e, exc_info=True)
+    logger.info('Done!')
+
+
+if __name__ == '__main__':
+    main()
+
--- test/test_clustering_algorithms.py
+++ test/test_clustering_algorithms.py
@@ -18,16 +18,23 @@ class DataForAlgorithms:
     k: int
     eps: float
     expected_started_graph: List[np.ndarray] = field(init=False)
+    expected_get_arc_weight: dict = field(init=False)
+    expected_k_graph: List[np.ndarray] = field(init=False)
+    expected_clustering_result: np.ndarray = field(init=False)
 
     def __post_init__(self):
         self.expected_started_graph = self.read_json_file(self.get_file_name('started_graph'))
+        self.expected_get_arc_weight = self.read_json_file(self.get_file_name('get_arc_weight'))
+        self.expected_k_graph = self.read_json_file(self.get_file_name('k_graph'))
+        self.expected_clustering_result = np.array(self.read_json_file(self.get_file_name('clustering_result')))
 
     def read_json_file(self, file_name):
         with open(file_name) as file:
             return json.loads(file.read())
 
     def get_file_name(self, file_name_suffix):
-        return os.path.join('./resources/', f'{self.name}_{file_name_suffix}.json')
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(base_dir, 'resources/', f'{self.name}_{file_name_suffix}.json')
 
 
 blobs_coord = sklearn.datasets.make_blobs(n_samples=50, random_state=0, cluster_std=0.5)[0]
@@ -83,4 +90,55 @@ class TestK_MXT:
             diff_elements = set(alg.start_graph[i]) ^ set(expected[i])
             assert not diff_elements
 
+    @pytest.mark.parametrize('x_init, y_init, k, eps, expected',[
+        (blob_data.x_init, blob_data.y_init, blob_data.k, blob_data.eps, blob_data.expected_get_arc_weight),
+        (circle_data.x_init, circle_data.y_init, circle_data.k, circle_data.eps, circle_data.expected_get_arc_weight),
+        (moon_data.x_init, moon_data.y_init, moon_data.k, moon_data.eps, moon_data.expected_get_arc_weight),
+    ])
+    def test_get_arc_weight(self, x_init, y_init, k, eps, expected):
+        def get_arc_name(v1, v2):
+            return f'({v1}, {v2})'
+
+        clusters = ClustersDataSpace2d(x_init=x_init, y_init=y_init, metrics='euclidean')
+        alg = K_MXT(k=k, eps=eps, clusters_data=clusters)
+        alg.make_start_graph()
+        for v in range(alg.num_of_vertices):
+            for to in range(alg.num_of_vertices):
+                assert alg.get_arc_weight(v, to) == expected[get_arc_name(v, to)]
+                assert alg.get_arc_weight(v, to) == expected[get_arc_name(to, v)]
+        # with open('./resources/moon_get_arc_weight.json', 'w') as file:
+        #     json.dump(weights, file)
+        # raise NotImplementedError
+
+    @pytest.mark.parametrize('x_init, y_init, k, eps, expected', [
+        (blob_data.x_init, blob_data.y_init, blob_data.k, blob_data.eps, blob_data.expected_k_graph),
+        (circle_data.x_init, circle_data.y_init, circle_data.k, circle_data.eps, circle_data.expected_k_graph),
+        (moon_data.x_init, moon_data.y_init, moon_data.k, moon_data.eps, moon_data.expected_k_graph),
+    ])
+    def test_make_k_graph(self, x_init, y_init, k, eps, expected):
+        clusters = ClustersDataSpace2d(x_init=x_init, y_init=y_init, metrics='euclidean')
+        alg = K_MXT(k=k, eps=eps, clusters_data=clusters)
+        alg.make_start_graph()
+        alg.make_k_graph()
+        assert len(alg.k_graph) == len(expected)
+        for i in range(len(expected)):
+            curr_expected_set = set(expected[i])
+            assert len(curr_expected_set) == len(alg.k_graph[i]) == len(expected[i])
+            diff_elements = set(alg.k_graph[i]) ^ set(expected[i])
+            assert not diff_elements
+        # with open('./resources/moon_k_graph.json', 'w') as file:
+        #     json.dump([row.tolist() for row in alg.k_graph], file)
+
+    @pytest.mark.parametrize('x_init, y_init, k, eps, expected', [
+        (blob_data.x_init, blob_data.y_init, blob_data.k, blob_data.eps, blob_data.expected_clustering_result),
+        (circle_data.x_init, circle_data.y_init, circle_data.k, circle_data.eps, circle_data.expected_clustering_result),
+        (moon_data.x_init, moon_data.y_init, moon_data.k, moon_data.eps, moon_data.expected_clustering_result),
+    ])
+    def test_clustering_result(self, x_init, y_init, k, eps, expected):
+        clusters = ClustersDataSpace2d(x_init=x_init, y_init=y_init, metrics='euclidean')
+        alg = K_MXT(k=k, eps=eps, clusters_data=clusters)
+        alg()
+        np.testing.assert_array_equal(alg.clusters_data.cluster_numbers, expected)
+        # with open('./resources/moon_clustering_result.json', 'w') as file:
+        #     json.dump(alg.clusters_data.cluster_numbers.tolist(), file)
 
